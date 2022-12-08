0fce83e933ff124a702918486056e0e0c2af39b4
ViktorLindgren95 <viktor.lindgren@snowsoftware.com>
2022-12-08 14:56:02 +0100

    Kept messing around, now with some schemas

--- main.tf
+++ main.tf
@@ -1,30 +1,41 @@
-module "repo1" {
+#locals {
+#  github_repos = yamldecode(file("repos.yaml"))
+#  flattened_repositories = flatten([
+#  for repo in local.github_repos : [
+#  for n in repo.topics : {
+#      topic    = n
+#  }
+#  ]
+#  ])
+#}
+locals {
+  github_repos= yamldecode(file("repos.yaml"))
+  config_privileges = distinct(flatten([
+  for each_repo in local.github_repos : [
+  for team in each_repo.teams : {
+    team_id = team
+    repo = each_repo.name
+  }
+  ]]))
+}
+output "repos" {
+  value = yamldecode(file("repos.yaml"))
+}
+output "repos2" {
+  value = local.config_privileges
+}
+module "repo" {
+  for_each = { for repo in local.github_repos : repo.name => repo }
   source             = "./github-repo"
-  repo_name          = "repo1"
-  visibility         = "public"
+  repo_name          = each.value.name
+  visibility         = each.value.visibility
   archive_on_destroy = false
 }
-module "repo2" {
-  source             = "./github-repo"
-  repo_name          = "repo2"
-  visibility         = "public"
-  archive_on_destroy = false
-}
-
-
-# Override default branch workaround
 resource "github_branch" "development" {
-  repository    = module.repo2.repo_instance
+  repository    = module.repo["Repo2"].repo_instance
   branch        = "development"
   source_branch = "main"
 }
-
-resource "github_branch_default" "default" {
-  repository = module.repo2.repo_instance
-  branch     = github_branch.development.branch
-}
-
-
 # TF_VAR_sonar_token
 variable "sonar_token" {
   description = "SonarQube token"
