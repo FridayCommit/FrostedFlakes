b65b40c46e34c71f621988764ab45cc2326c6180
ViktorLindgren95 <viktor.lindgren@snowsoftware.com>
2022-12-09 10:56:10 +0100

    Moved things to variables. Use a terraform.tfvars file to make this easier during development
    added sonarqube provider.

--- main.tf
+++ main.tf
@@ -1,43 +1,41 @@
-#locals {
-#  github_repos = yamldecode(file("repos.yaml"))
-#  flattened_repositories = flatten([
-#  for repo in local.github_repos : [
-#  for n in repo.topics : {
-#      topic    = n
-#  }
-#  ]
-#  ])
-#}
 locals {
-  github_repos = yamldecode(file("repos.yaml"))
-  github_teams = yamldecode(file("teams.yaml"))
+  github_repos      = yamldecode(file("repos.yaml"))
+  github_teams      = yamldecode(file("teams.yaml"))
   repo_team_binding = distinct(flatten([
-    for each_repo in local.github_repos : [
-      for team in each_repo.teams : {
-        team_id = team.name
-        repo    = each_repo.name
-        team_permission = team.permission
-      }
-  ]]))
+  for each_repo in local.github_repos : [
+  for team in each_repo.teams : {
+    team_id         = team.name
+    repo            = each_repo.name
+    team_permission = team.permission
+  }
+  ]
+  ]))
 }
 
 module "repo" {
-  for_each           = { for repo in local.github_repos : repo.name => repo } # Sets the index if i understand
+  for_each           = {for repo in local.github_repos : repo.name => repo} # Sets the index if i understand
   source             = "./github-repo"
   repo_name          = each.value.name
   visibility         = each.value.visibility
-  extra_members = each.value.extra-members
+  extra_members      = each.value.extra-members
   archive_on_destroy = false
 }
+resource "sonarqube_project" "main" {
+  for_each = module.repo
+  name       = each.value.repo_instance
+  project    = each.value.repo_instance
+  visibility = "public"
+}
+
 
 resource "github_team" "teams" {
-  for_each    = { for team in local.github_teams : team.name => team } # Sets the index if i understand
+  for_each    = {for team in local.github_teams : team.name => team} # Sets the index if i understand
   name        = each.value.name
   description = each.value.description
   privacy     = each.value.privacy
 }
 resource "github_team_repository" "team-bind" {
-  for_each   = { for team in local.repo_team_binding : "${team.repo}-${team.team_id}" => team }
+  for_each   = {for team in local.repo_team_binding : "${team.repo}-${team.team_id}" => team}
   team_id    = each.value.team_id
   repository = each.value.repo
   permission = each.value.team_permission
@@ -51,29 +49,15 @@ resource "github_branch" "development" {
 }
 
 
-
-
-
-
-
-# TF_VAR_sonar_token
-variable "sonar_token" {
-  description = "SonarQube token"
-  type        = string
-}
 resource "github_actions_organization_secret" "SONAR_TOKEN" {
   secret_name     = "SONAR_TOKEN"
   visibility      = "private"
-  encrypted_value = var.sonar_token
+  encrypted_value = base64encode(var.sonar_token)
 }
 
-# TF_VAR_sonar_url
-variable "sonar_url" {
-  description = "SonarQube url"
-  type        = string
-}
+
 resource "github_actions_organization_secret" "SONAR_URL" {
   secret_name     = "SONAR_URL"
   visibility      = "private"
-  encrypted_value = var.sonar_url
+  encrypted_value = base64encode(var.sonar_url)
 }
--- provider.tf
+++ provider.tf
@@ -4,17 +4,25 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.0"
     }
+    sonarqube = {
+      source  = "jdamata/sonarqube"
+      version = "0.15.3"
+    }
   }
 }
 
-# TF_VAR_gh_token
-variable "gh_token" {
-  description = "GitHub token"
-  type        = string
-}
-
 provider "github" {
-  owner = "FridayCommit"
+  owner = var.gh_org
   token = var.gh_token
 }
-
+#provider "github" {
+#  app_auth {
+#    id              = var.app_id              # or `GITHUB_APP_ID`
+#    installation_id = var.app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
+#    pem_file        = var.app_pem_file        # or `GITHUB_APP_PEM_FILE`
+#  }
+#}
+provider "sonarqube" {
+  token = var.sonar_admin_token
+  host  = var.sonar_url
+}
