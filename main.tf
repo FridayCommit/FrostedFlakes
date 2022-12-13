a0539ab13c07e575fd71dd10a531c45947b606f5
ViktorLindgren95 <viktor.lindgren@snowsoftware.com>
2022-12-13 11:14:22 +0100

    Restructure

--- main.tf
+++ main.tf
@@ -19,6 +19,22 @@ locals {
   ]
   ]))
 }
+resource "github_team" "teams" {
+  for_each    = {for team in local.github_teams : team.name => team} # Sets the index if i understand
+  name        = each.value.name
+  description = each.value.description
+  privacy     = each.value.privacy
+}
+# Add this https://registry.terraform.io/providers/integrations/github/latest/docs/resources/team_members
+# Figure out how to do it as a for_each, looks nested
+#https://medium.com/@larry_nguyen/how-to-repeat-resources-block-in-terraform-using-loops-and-variables-36b13726d638
+resource "github_team_membership" "team_membership" {
+  for_each   = {for team-membership in local.member_team_binding : "${team-membership.team_id}-${team-membership.username}" => team-membership}
+  team_id    = github_team.teams[each.value.team_id].id
+  username   = each.value.username
+  role       = "member"
+  depends_on = [github_team.teams]
+}
 
 module "repo" {
   for_each           = {for repo in local.github_repos : repo.name => repo} # Sets the index if i understand
@@ -26,10 +42,12 @@ module "repo" {
   repo_name          = each.value.name
   visibility         = each.value.visibility
   extra_members      = each.value.extra-members
+  topics             = each.value.topics
   archive_on_destroy = true
+  depends_on = [github_team.teams]
 }
 data "github_repository" "repo_data" {
-  for_each   = module.repo
+  for_each  = module.repo
   full_name = each.value.repo_instance.full_name
 }
 resource "sonarqube_project" "project" {
@@ -41,9 +59,10 @@ resource "sonarqube_project" "project" {
 # TODO we could reference maybe something like ${each.value.repo_instance.default_branch}. i know its decrecated but we arent using it to set, but to retrieve? Or data source?
 # I dunno yet if the data source also works as depends on or if i have to be explicit here
 data "http" "set_branch" {
-  for_each = module.repo
-  url    = "${var.sonar_url}/api/project_branches/rename?name=${data.github_repository.repo_data[each.value.repo_instance.name].default_branch}&project=${each.value.repo_instance.name}" //TODO felix fatta hur vi passar branch
-  method = "POST"
+  for_each        = module.repo
+  url             = "${var.sonar_url}/api/project_branches/rename?name=${data.github_repository.repo_data[each.value.repo_instance.name].default_branch}&project=${each.value.repo_instance.name}"
+  //TODO felix fatta hur vi passar branch
+  method          = "POST"
   request_headers = {
     Authorization = "Basic ${base64encode(format("%s:",var.sonar_admin_token))}"
   }
@@ -56,9 +75,10 @@ data "http" "set_branch" {
   depends_on = [sonarqube_project.project]
 }
 data "http" "set_alm" {
-  for_each = module.repo
-  url    = "${var.sonar_url}/api/alm_settings/set_github_binding?almSetting=GitHub&monorepo=no&project=${each.value.repo_instance.name}&repository=${each.value.repo_instance.full_name}" //TODO felix fatta hur vi passar branch
-  method = "POST"
+  for_each        = module.repo
+  url             = "${var.sonar_url}/api/alm_settings/set_github_binding?almSetting=GitHub&monorepo=no&project=${each.value.repo_instance.name}&repository=${each.value.repo_instance.full_name}"
+  //TODO felix fatta hur vi passar branch
+  method          = "POST"
   request_headers = {
     Authorization = "Basic ${base64encode(format("%s:",var.sonar_admin_token))}"
   }
@@ -78,28 +98,13 @@ data "http" "set_alm" {
 # https://registry.terraform.io/providers/integrations/github/latest/docs/data-sources/repository
 # Above is a data-source that seem to know what the default branch is
 # https://developer.hashicorp.com/terraform/language/data-sources
-resource "github_team" "teams" {
-  for_each    = {for team in local.github_teams : team.name => team} # Sets the index if i understand
-  name        = each.value.name
-  description = each.value.description
-  privacy     = each.value.privacy
-}
-# Add this https://registry.terraform.io/providers/integrations/github/latest/docs/resources/team_members
-# Figure out how to do it as a for_each, looks nested
-#https://medium.com/@larry_nguyen/how-to-repeat-resources-block-in-terraform-using-loops-and-variables-36b13726d638
-resource "github_team_membership" "some_team_membership" {
-  for_each = {for team-membership in local.member_team_binding : "${team-membership.team_id}-${team-membership.username}" => team-membership}
-  team_id  = github_team.teams[each.value.team_id].id
-  username = each.value.username
-  role     = "member"
-  depends_on = [resource.github_team.teams]
-}
-resource "github_team_repository" "team-bind" { # TODO Should this be moved to the github module? Its part of the repository in a way right ? Just make sure teams are created before repos when strap
+resource "github_team_repository" "team_bind" {
+  # TODO Should this be moved to the github module? Its part of the repository in a way right ? Just make sure teams are created before repos when strap
   for_each   = {for team in local.repo_team_binding : "${team.repo}-${team.team_id}" => team}
   team_id    = each.value.team_id
   repository = each.value.repo
   permission = each.value.team_permission
-  depends_on = [resource.github_team.teams,module.repo]
+  depends_on = [resource.github_team.teams, module.repo]
 }
 
 resource "github_actions_organization_secret" "SONAR_TOKEN" {
--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,6 @@
 #output "repo_team_binding" {
 #  value = local.repo_team_binding
 #}
-output "member_team_binding" {
-  value = local.member_team_binding
-}
+#output "member_team_binding" {
+#  value = local.member_team_binding
+#}
